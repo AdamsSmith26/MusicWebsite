166add7781a316bb3f808fcb48116cf7e023fc56
Adam <adamssmith2003@gmail.com>
2023-04-08 19:31:38 +0400

    v6

--- script.js
+++ script.js
@@ -6,43 +6,43 @@ const songs = [
         id: 1,
         songName: `I Wanna Be Yours <br> 
         <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        poster: "img/1.jpg"
     },
     {
         id: 2,
         songName: `Here <br> 
         <div class="subtitle">Alessia Cara</div>`,
-        poster: "img/Here.jpg"
+        poster: "img/2.jpg"
     },
     {
         id: 3,
-        songName: `I Wanna Be Yours <br> 
-        <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        songName: `Escapism <br> 
+        <div class="subtitle">Raye</div>`,
+        poster: "img/3.jpg"
     },
     {
         id: 4,
-        songName: `I Wanna Be Yours <br> 
-        <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        songName: `Muddy Waters <br> 
+        <div class="subtitle">LP</div>`,
+        poster: "img/4.jpg"
     },
     {
         id: 5,
-        songName: `I Wanna Be Yours <br> 
-        <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        songName: `Happiness is a Butterfly <br> 
+        <div class="subtitle">Lana Del Rey</div>`,
+        poster: "img/5.jpg"
     },
     {
         id: 6,
-        songName: `I Wanna Be Yours <br> 
-        <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        songName: `Diet Mountain Dew <br> 
+        <div class="subtitle">Lana Del Rey</div>`,
+        poster: "img/6.jpg"
     },
     {
         id: 7,
         songName: `I Wanna Be Yours <br> 
         <div class="subtitle">Arctic Monkeys</div>`,
-        poster: "img/IWannaBeYours.png"
+        poster: "img/DietMountainDewLanaDelRey.jpg"
     },
     {
         id: 8,
@@ -122,11 +122,51 @@ const songs = [
         <div class="subtitle">Arctic Monkeys</div>`,
         poster: "img/IWannaBeYours.png"
     }
-]
+];
 
 Array.from(document.getElementsByClassName('songItem')).forEach((e, i) => {
     e.getElementsByTagName('img')[0].src = songs[i].poster;
+    e.getElementsByTagName('h5')[0].innerHTML = songs[i].songName;
 });
+
+
+let masterPlay = document.getElementById('masterPlay');
+let wave = document.getElementById('wave')
+
+masterPlay.addEventListener('click', () => {
+    if (music.paused || music.currentTime <= 0) {
+        music.play();
+        wave.classList.add('active1');
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+
+    } else {
+        music.pause();
+        wave.classList.remove('active1');
+        masterPlay.classList.add('bi-play-fill');
+        masterPlay.classList.remove('bi-pause-fill');
+    }
+})
+
+let index = 0;
+let poster_master_play = document.getElementById('poster_master_play');
+
+Array.from(document.getElementsByClassName('playListPlay')).forEach((e) => {
+    e.addEventListener('click', (el) => {
+        let index = el.target.id;
+        music.src = `music/${index}.mp3`;
+        poster_master_play.src = `img/${index}.jpg`;
+        music.play();
+        masterPlay.classList.remove('bi-play-fill');
+        masterPlay.classList.add('bi-pause-fill');
+        // console.log(index);
+    })
+})
+
+
+
+
+
 
 
 let pop_song_left = document.getElementById('pop_song_left');
